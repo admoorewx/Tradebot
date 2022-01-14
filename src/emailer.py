4ac25f816ad5eb86bf4572ada407944ee93c56bb
Andrew Moore <41948559+admoorewx@users.noreply.github.com>
2022-01-14 16:46:27 -0100

    Remove personal email information

--- src/emailer.py
+++ src/emailer.py
@@ -4,14 +4,14 @@ from email.mime.text import MIMEText
 
 def send_email(content):
     message = MIMEMultipart()
-    message["From"] = "gcetradebot@gmail.com"
-    message["To"] = "admoorewx@gmail.com"
+    message["From"] = ""
+    message["To"] = ""
     message["Subject"] = "Daily Tradebot Report"
     message.attach(MIMEText(content, "plain"))
     message = message.as_string()
     port = 465
-    password = "Tradebot1!"
+    password = ""
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login("gcetradebot@gmail.com",password)
-        server.sendmail("gcetradebot@gmail.com","admoorewx@gmail.com",message)
+        server.login("",password)
+        server.sendmail("","",message)
